e7317a064d73788582255a951e480fa4b6629cb2
dagmara <dagmara.gabriela.stach@gmail.com>
2023-03-10 18:59:59 +0100

    Add Logo.js,  Logo.css, Navigation.js

---
+++ smart-brain-app/src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+
+const Logo = () =>{
+    return (
+    <div className='ma4 mt0'>
+        <Tilt>
+            <div className='br2 shadow-5 Tilt' style={{ height: '150px', width:'150px', backgroundColor: 'darkgreen' }}>
+                <h1>React Parallax Tilt 👀</h1>
+            </div>
+        </Tilt>
+        </div>
+    )
+}
+export default Logo
