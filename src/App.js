c0438321d53d432ca529eec591da3f062c83703c
dagmara <dagmara.gabriela.stach@gmail.com>
2023-05-10 23:15:54 +0200

    connect with backend after backend deploy

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://smartbrainappbackend.onrender.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -82,7 +82,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response.status.description === "Ok") {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://smartbrainappbackend.onrender.com:3000/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
